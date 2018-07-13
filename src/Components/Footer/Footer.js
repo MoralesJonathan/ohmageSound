03b3dd1820a1c23391b934f8e7b4437815dd599c
Jonathan Morales <jjm15c@my.fsu.edu>
2018-07-13 12:53:52 -0400

    Add footer and content

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,14 @@ import './Footer.css';
 class Footer extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <footer>
+        <div className="socialMediaIconsWrapper">
+          <a href="https://www.facebook.com/Ohamge-Sound-207050473476335/"><img src="socialMedia/facebook/2.svg" alt="Facebook" /></a>
+          <a href="https://www.instagram.com/ohmagesound/"><img src="socialMedia/instagram/1.png" alt="Instagram"/></a>
+          <a href="https://www.youtube.com/channel/UCX_vmrVfLxt_apHK8k9A5dQ"><img src="socialMedia/youtube/2.png" alt="Youtube"/></a>
+        </div>
+        <span>&copy; 2018 Ohmage Sound LLC</span>
+      </footer>
     );
   }
 }
