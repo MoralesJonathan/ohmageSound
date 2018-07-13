1a81b9af2a27f73d1dbfc34f88c0949c8a01e3d2
Jonathan Morales <jjm15c@my.fsu.edu>
2018-07-13 12:09:22 -0400

    Create contact section

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,7 +6,22 @@ class Contact extends Component {
   render() {
     return (
       <div className="container">
-        <SectionTitle/>
+        <SectionTitle title="Want to reach us?"/>
+        <form id="contactForm">
+          <div className="row">
+            <div className="six columns">
+              <label htmlFor="nameInput">Your Name</label>
+              <input className="u-full-width" type="text" placeholder="John Smith" id="nameInput"/>
+              <label htmlFor="emailInput">Your Email</label>
+              <input className="u-full-width" type="email" placeholder="Smith@mailbox.com" id="emailInput"/>
+            </div>
+            <div className="six columns">
+              <label htmlFor="messageInput">Message</label>
+              <textarea className="u-full-width" placeholder="Write your message here!" id="messageInput"/>
+            </div>
+          </div>
+          <input className="button-primary u-pull-right button" type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
