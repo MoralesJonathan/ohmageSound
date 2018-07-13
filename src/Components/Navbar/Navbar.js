271d6ec53636423e191de30859522503e72ec711
Jonathan Morales <jjm15c@my.fsu.edu>
2018-07-13 14:18:41 -0400

    Add responsiveness

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,11 +8,15 @@ class Navbar extends Component {
       <nav>
         <div className="navContainer">
           <img className="navlogo" src="http://via.placeholder.com/100x100" />
+          <input type="checkbox" id="navMobile" class="hidden"/>
+          <label for="navMobile" className="navMobile-open"><i></i><i></i><i></i></label>
+          <div className="navMenuContainer">
           <ul className="navMenu">
             <NavMenuItem title="Home" link="/"/>
             <NavMenuItem title="About Us" link="#"/>
             <NavMenuItem title="Contact Us" link="#"/>
           </ul>
+          </div>
         </div>
       </nav>
     );
