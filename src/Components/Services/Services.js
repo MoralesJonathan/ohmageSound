4349ae4033d927145669fdb2c53c0b023dc1c611
Jonathan Morales <jjm15c@my.fsu.edu>
2018-07-13 22:43:19 -0400

    change service paragraph and icons

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -10,9 +10,9 @@ class Services extends Component {
         <SectionTitle title="What we offer"/>
         <div className="row">
           <div className="twelve columns cardContainer">
-            <ServiceItem icon="memory" title="Equipment Rental" paragraph="Lorem ipsum dolor sit amet consectetur adipiscing elit. Vestibulum dictum nisi erat, id tincidunt elit vulputate et."/>
-            <ServiceItem icon="memory" title="DJ Services" paragraph="Lorem ipsum dolor sit amet consectetur adipiscing elit. Vestibulum dictum nisi erat, id tincidunt elit vulputate et."/>
-            <ServiceItem icon="memory" title="Events" paragraph="Lorem ipsum dolor sit amet consectetur adipiscing elit. Vestibulum dictum nisi erat, id tincidunt elit vulputate et."/>
+            <ServiceItem icon="speaker_group" title="Rentals" paragraph="Rent our equipment hourly. Equipment can include speakers, lighting, projector screen and projectors."/>
+            <ServiceItem icon="supervisor_account" title="DJ Services" paragraph="Hire us to DJ your next event! We have different pricing tiers to suit your needs whether its a small birthday or large party!."/>
+            <ServiceItem icon="location_city" title="Events" paragraph="Our events package is a current work in progress to please contact us to recieve personal quotes and more information."/>
           </div>
         </div>
       </div>
