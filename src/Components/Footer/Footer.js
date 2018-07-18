fecc38f7970f070aa79501c41d84934e9bff441e
Jonathan Morales <jonmorazav@yahoo.com>
2018-07-18 01:02:37 -0400

    Add target _blank for footer links and fix fb link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -20,9 +20,9 @@ class Footer extends Component {
     return (
       <footer>
         <div className="socialMediaIconsWrapper">
-          <a onClick={this.handleClick} href="https://www.facebook.com/Ohamge-Sound-207050473476335/"><img data-social-media-page="Faceook" src="socialMedia/facebook/2.svg" alt="Facebook" /></a>
-          <a onClick={this.handleClick} href="https://www.instagram.com/ohmagesound/"><img data-social-media-page="Instagram" src="socialMedia/instagram/1.png" alt="Instagram"/></a>
-          <a onClick={this.handleClick} href="https://www.youtube.com/channel/UCX_vmrVfLxt_apHK8k9A5dQ"><img data-social-media-page="Youtube" src="socialMedia/youtube/2.png" alt="Youtube"/></a>
+          <a onClick={this.handleClick} target="_blank" href="https://www.facebook.com/ohmagesound/"><img data-social-media-page="Faceook" src="socialMedia/facebook/2.svg" alt="Facebook" /></a>
+          <a onClick={this.handleClick} target="_blank" href="https://www.instagram.com/ohmagesound/"><img data-social-media-page="Instagram" src="socialMedia/instagram/1.png" alt="Instagram"/></a>
+          <a onClick={this.handleClick} target="_blank" href="https://www.youtube.com/channel/UCX_vmrVfLxt_apHK8k9A5dQ"><img data-social-media-page="Youtube" src="socialMedia/youtube/2.png" alt="Youtube"/></a>
         </div>
         <span>&copy; 2018 Ohmage Sound LLC</span>
       </footer>
